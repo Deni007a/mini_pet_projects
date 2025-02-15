3adc0d369a39985afae4db59ea06780be7ca8acb
DenisEsipko <iesypko@gmail.com>
2025-02-15 21:29:03 +0200

    "build project Chart fix url"

--- chart-project/vite.config.js
+++ chart-project/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Deni007a/mini_pet_projects/chart-project/',
+  //base: '/Deni007a/mini_pet_projects/chart-project/',
+  base: '/mini_pet_projects/',
   plugins: [react()],
 })
